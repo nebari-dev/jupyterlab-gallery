936b0fe383ec32ccc7f77fc92f260ab0ab0d1dce
Michał Krassowski <5832902+krassowski@users.noreply.github.com>
2024-07-01 10:18:47 +0100

    Add placeholder exhibit icon (#31)
    * Add placeholder icon
    
    * Add edge case examples to binder
    
    * Lint

--- src/icons.ts
+++ src/icons.ts
@@ -1,7 +1,13 @@
 import { LabIcon } from '@jupyterlab/ui-components';
 import gallerySvgstr from '../style/icons/md/gallery.svg';
+import repositorySvgstr from '../style/icons/md/repository.svg';
 
 export const galleryIcon = new LabIcon({
   name: 'jupyterlab-gallery:gallery',
   svgstr: gallerySvgstr
+});
+
+export const repositoryIcon = new LabIcon({
+  name: 'jupyterlab-gallery:repository',
+  svgstr: repositorySvgstr
 });
