344af5cd56824619eab89c876b074c4baee35656
Michał Krassowski <5832902+krassowski@users.noreply.github.com>
2024-07-09 10:13:03 +0100

    Expose `branch` and `depth` options (#35)
    * Expose `branch` and `depth` options
    
    * Bump version to 0.4.0

--- jupyterlab_gallery/tests/test_handlers.py
+++ jupyterlab_gallery/tests/test_handlers.py
@@ -14,24 +14,30 @@ async def test_exhibits(jp_fetch):
     assert isinstance(payload["exhibits"], list)
 
 
-@pytest.mark.parametrize("exhibit", [
-    {
-        "git": "https://github.com/nebari-dev/nebari.git",
-        "homepage": "https://github.com/nebari-dev/nebari"
-    },
-    {
-        "git": "https://github.com/nebari-dev/nebari.git",
-        "homepage": "https://github.com/nebari-dev/nebari",
-        "icon": None
-    }
-])
+@pytest.mark.parametrize(
+    "exhibit",
+    [
+        {
+            "git": "https://github.com/nebari-dev/nebari.git",
+            "homepage": "https://github.com/nebari-dev/nebari",
+        },
+        {
+            "git": "https://github.com/nebari-dev/nebari.git",
+            "homepage": "https://github.com/nebari-dev/nebari",
+            "icon": None,
+        },
+    ],
+)
 async def test_exhibit_generate_github_icon(jp_serverapp, jp_fetch, exhibit):
-    with mock.patch.object(GalleryManager, 'exhibits', [exhibit]):
+    with mock.patch.object(GalleryManager, "exhibits", [exhibit]):
         response = await jp_fetch("jupyterlab-gallery", "exhibits")
     assert response.code == 200
     payload = json.loads(response.body)
     assert len(payload["exhibits"]) == 1
-    assert payload["exhibits"][0]["icon"] == 'https://opengraph.githubassets.com/1/nebari-dev/nebari'
+    assert (
+        payload["exhibits"][0]["icon"]
+        == "https://opengraph.githubassets.com/1/nebari-dev/nebari"
+    )
 
 
 async def test_gallery(jp_fetch):
@@ -41,16 +47,15 @@ async def test_gallery(jp_fetch):
     assert payload["apiVersion"] == "1.0"
 
 
-async def test_pull_token_can_be_used_instead_of_xsrf(jp_serverapp, jp_base_url, http_server_client):
+async def test_pull_token_can_be_used_instead_of_xsrf(
+    jp_serverapp, jp_base_url, http_server_client
+):
     token = jp_serverapp.identity_provider.token
     response = await http_server_client.fetch(
         url_path_join(jp_base_url, "jupyterlab-gallery", "pull"),
         body=b'{"exhibit_id": 100}',
         method="POST",
-        headers={
-            "Authorization": f"token {token}",
-            "Cookie": ""
-        },
+        headers={"Authorization": f"token {token}", "Cookie": ""},
         raise_error=False,
     )
     assert response.code == 406
