7d1dc218a0dcccb7fa87bd2c721ca98fad7e2cb5
krassowski <5832902+krassowski@users.noreply.github.com>
2024-07-26 11:23:03 +0100

    Adopt to run as a standalone app

--- jupyterlab_gallery/app.py
+++ jupyterlab_gallery/app.py
@@ -1,4 +1,5 @@
 from jupyter_server.extension.application import ExtensionApp
+from jupyter_server.serverapp import ServerApp
 from .handlers import ExhibitsHandler, GalleryHandler, PullHandler
 from .manager import GalleryManager
 
@@ -12,9 +13,8 @@ class GalleryApp(ExtensionApp):
         ("jupyterlab-gallery/pull", PullHandler),
     ]
 
-    # default_url = "/jupyterlab-gallery"
-    # load_other_extensions = True
-    # file_url_prefix = "/gallery"
+    default_url = "/jupyterlab-gallery/gallery"
+    load_other_extensions = False
 
     def initialize_settings(self):
         self.log.info("Configured gallery manager")
@@ -28,3 +28,16 @@ class GalleryApp(ExtensionApp):
         self.serverapp.web_app.settings["nbapp"] = self.serverapp
 
         self.log.info(f"Registered {self.name} server extension")
+
+    @classmethod
+    def make_serverapp(cls, **kwargs) -> ServerApp:
+        """Instantiate the ServerApp
+
+        Override to disable default_services which would give access
+        to files on the disk (`contents` service) or allow execution
+        code (`kernels` service).
+        """
+        server_app = super().make_serverapp(**kwargs)
+        assert len(server_app.default_services) > 1
+        server_app.default_services = ("auth", "security")
+        return server_app
